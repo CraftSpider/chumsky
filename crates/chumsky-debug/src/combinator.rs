90112cd4b781535c600b4a3f8efdf8b6bcdcfd49
Rune Tynan <runetynan@gmail.com>
2023-03-10 12:24:51 -0500

    Combinator

--- crates/chumsky-debug/src/combinator.rs
+++ crates/chumsky-debug/src/combinator.rs
@@ -1,11 +1,11 @@
 use chumsky::input::InputRef;
 use super::*;
 
-pub struct DebugOffset<A> {
-    pub(crate) inner: A,
+pub struct DebugOffset<'a, A> {
+    pub(crate) parser: A,
 }
 
-impl<'a, A, I, O, E> ExtParser<'a, I, O, E> for DebugOffset<A>
+impl<'a, A, I, O, E> ExtParser<'a, I, O, E> for DebugOffset<'a, A>
 where
     A: Parser<'a, I, O, E>,
     I: Input<'a>,
@@ -13,16 +13,61 @@ where
     E: ParserExtra<'a, I>,
 {
     fn parse(&self, inp: &mut InputRef<'a, '_, I, E>) -> Result<O, E::Error> {
-        dbg!(inp.offset());
-        match inp.parse(&self.inner) {
+        println!("Starting Offset: {:?}", inp.offset());
+        match inp.parse(&self.parser) {
             Ok(out) => {
-                dbg!(inp.offset());
+                println!("Final Offset: {:?}", inp.offset());
                 Ok(out)
             }
             Err(e) => {
                 println!("Error Occurred");
                 Err(e)
             }
+        }
+    }
+}
+
+pub struct DebugOut<A> {
+    pub(crate) parser: A,
+}
+
+impl<'a, A, I, O, E> ExtParser<'a, I, O, E> for DebugOut<A>
+where
+    A: Parser<'a, I, O, E>,
+    I: Input<'a>,
+    O: Debug,
+    E: ParserExtra<'a, I>,
+{
+    fn parse(&self, inp: &mut InputRef<'a, '_, I, E>) -> Result<O, E::Error> {
+        match inp.parse(&self.parser) {
+            Ok(out) => {
+                println!("Parser Output: {:?}", out);
+                Ok(out)
+            }
+            Err(e) => Err(e),
+        }
+    }
+}
+
+pub struct DebugSpan<A> {
+    pub(crate) parser: A,
+}
+
+impl<'a, A, I, O, E> ExtParser<'a, I, O, E> for DebugSpan<A>
+where
+    A: Parser<'a, I, O, E>,
+    I: Input<'a>,
+    I::Span: Debug,
+    E: ParserExtra<'a, I>,
+{
+    fn parse(&self, inp: &mut InputRef<'a, '_, I, E>) -> Result<O, E::Error> {
+        let before = inp.offset();
+        match inp.parse(&self.parser) {
+            Ok(out) => {
+                println!("Consumed Span: {:?}", inp.span_since(before));
+                Ok(out)
+            },
+            Err(e) => Err(e),
         }
     }
 }
--- crates/chumsky-debug/src/lib.rs
+++ crates/chumsky-debug/src/lib.rs
@@ -1,4 +1,5 @@
 use std::fmt::Debug;
+use std::io::Write;
 use chumsky::{
     input::Input,
     extra::ParserExtra,
@@ -10,18 +11,44 @@ mod combinator;
 
 use combinator::*;
 
+pub enum Target<'a> {
+    Out,
+    Err,
+    Write(&'a dyn Write),
+}
+
 pub trait DebugParser<'a, I, O, E>: Parser<'a, I, O, E>
 where
     I: Input<'a>,
     E: ParserExtra<'a, I>,
 {
-    fn debug_offset(self) -> Ext<DebugOffset<Self>>
+    fn debug_offset(self) -> Ext<DebugOffset<'a, Self>>
     where
         Self: Sized,
         I::Offset: Debug,
     {
         Ext(DebugOffset {
-            inner: self,
+            parser: self,
+        })
+    }
+
+    fn debug_out(self) -> Ext<DebugOut<Self>>
+    where
+        Self: Sized,
+        O: Debug,
+    {
+        Ext(DebugOut {
+            parser: self,
+        })
+    }
+
+    fn debug_span(self) -> Ext<DebugSpan<Self>>
+    where
+        Self: Sized,
+        I::Span: Debug,
+    {
+        Ext(DebugSpan {
+            parser: self,
         })
     }
 }
